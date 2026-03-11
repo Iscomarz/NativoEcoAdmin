0581856980242c1fea179889a4d0727c73061186
Iscomarz <franmtz96@gmail.com>
2026-03-11 10:55:03 -0600

    prerender y ssr

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,4 @@
-export const prerender = true;
-export const ssr = false;
+// Por defecto, no prerrenderizamos ya que gran parte de la app es dinámica y está tras login
+export const prerender = false;
+// Habilitamos SSR para que las acciones de servidor funcionen correctamente en Vercel
+export const ssr = true;
